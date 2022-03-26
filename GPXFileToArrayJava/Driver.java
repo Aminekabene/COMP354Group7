da78f75881665e4a21ce63b1294eeae94b4934c2
Aminekabene <kabenea99@gmail.com>
2022-03-26 15:43:53 -0400

    asd

--- GPXFileToArrayJava/Driver.java
+++ GPXFileToArrayJava/Driver.java
@@ -1,15 +1,28 @@
 import java.util.List;
+import org.json.JSONArray;
+
 
 public class Driver {
+	
+	
+	public static void double[] arrToJson(double coordArr[]) {
+		JSONArray jsArray = new JSONArray();
+		
+	      for (int i = 0; i < coordArr.length; i++) {
+	         jsArray.put(coordArr[i]);
+	      }
+	      System.out.println(jsArray);
+	}
 
 	public static void main(String[] args) {
-		String GPXFileName="testTrack.gpx";
+		String GPXFileName="C:\\xampp\\htdocs\\COMP354Group7\\GPXFileToArrayJava\\testTrack.gpx";
 		
 		List<CoordFull> cf = GPXtoArray.convert(GPXFileName);
 		
 		Coord[] coordArr = Coord.coordArr(cf);
 		//we pass this coordArr to the javascript to output the
-
+		
+		
 
 
 		
